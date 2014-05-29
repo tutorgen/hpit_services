ef373e684b6cf5c086f1d5b05938ecf2fef8842d
Raymond Chandler III <raymondchandleriii@gmail.com>
2014-05-29 12:52:14 -0400

    Added a bind address for the hpit server to run on, allowing remote outside connections to the server for test purposes.

--- manager.py
+++ manager.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 import signal
 
-from server.settings import HPIT_PID_FILE
+from server.settings import HPIT_PID_FILE, HPIT_BIND_ADDRESS
 
 def read_configuration():
     try:
@@ -150,7 +150,7 @@ def start(arguments, configuration):
         print("The HPIT Server is already running.")
     else:
         print("Starting the HPIT Hub Server...")
-        subprocess.call(["gunicorn", "server:app", "--daemon", "--pid", HPIT_PID_FILE])
+        subprocess.call(["gunicorn", "server:app", "--bind", HPIT_BIND_ADDRESS, "--daemon", "--pid", HPIT_PID_FILE])
 
         print("Starting tutors...")
         spin_up_all('tutor', configuration)
--- server/settings.py
+++ server/settings.py
@@ -1,1 +1,2 @@
 HPIT_PID_FILE = 'tmp/hpit_server.pid'
+HPIT_BIND_ADDRESS = "0.0.0.0:8000"
