18961c99c0d0f66b7a45193e64c0514a73ffeb68
bmsauer <brianmsauer@gmail.com>
2014-06-12 11:27:38 -0400

    Tutor replay now semi functional, with filter and timestamp support

--- tutors/replay.py
+++ tutors/replay.py
@@ -8,6 +8,8 @@ from time import sleep
 import json
 import sys
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+import pytz
 
 from client import Tutor
 
@@ -19,23 +21,45 @@ class ReplayTutor(Tutor):
         self.args = json.loads(args[1:-1]) #trim off the quotes
         
     def main_callback(self):
-        #TODO: replay logic here
         client = MongoClient()
         messages = client.hpit_development.messages
-        for message in messages.find():
-            self.send(message["event"],message["payload"])
-            self.logger.debug("REPLAYED: "+str(message))
+        self.logger.debug("filter is: " + str(self.args["filter"]))
+        for message in messages.find(self.args["filter"]):
+            
+            criteriaFlag = 0
+            successFlag = 0
+            documentTime = ObjectId(message["_id"]).generation_time
+            documentTime = documentTime.replace(tzinfo=pytz.utc)
+            if "afterTime" in self.args:
+                criteriaFlag = criteriaFlag | 1 
+                afterTime  = datetime.strptime(self.args["afterTime"], "%m-%d-%Y %H:%M:%S")
+                afterTime = afterTime.replace(tzinfo=pytz.utc)
+                if afterTime <= documentTime:
+                    successFlag  = criteriaFlag | 1
+                    
+            if "beforeTime" in self.args:
+                criteriaFlag = criteriaFlag | 2
+                beforeTime = datetime.strptime(self.args["beforeTime"], "%m-%d-%Y %H:%M:%S")
+                beforeTime = beforeTime.replace(tzinfo=pytz.utc)
+                if beforeTime >= documentTime:
+                    successFlag = criteriaFlag | 2
+                    
+            if criteriaFlag == successFlag:
+                #self.send(message["event"],message["payload"])
+                self.logger.debug("REPLAYED: "+str(message))
             
         
         if self.run_once:
             return False
         else:
             return True
+   
         
-
     def run(self):
         self.connect()
         
         self.start()
         
         self.disconnect()
+        
+    
