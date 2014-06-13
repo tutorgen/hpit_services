6e07b1f28e4f94c4b4355551852e59d0d09092ed
bmsauer <brianmsauer@gmail.com>
2014-06-13 14:49:53 -0400

    Added message_sender text and tutor test

--- tests/client/mixins/test_message_sender_mixin.py
+++ tests/client/mixins/test_message_sender_mixin.py
@@ -3,9 +3,13 @@ import httpretty
 import json
 import pytest
 
-from client.mixins import RequestsMixin
 from client.mixins import MessageSenderMixin
 from client.exceptions import InvalidMessageNameException
+from client.exceptions import ResponseDispatchError
+from client.settings import HPIT_URL_ROOT
+
+def send_callback():
+    print("test callback")
 
 @httpretty.activate
 def test_send():
@@ -17,30 +21,42 @@ def test_send():
     """
     
     test_payload = {"item":"shoe"}
-    def test_callback():
-        print("test callback")
     
-    httpretty.register_uri(httpretty.POST,"http://localhost/message",
+    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/message",
                             body='{"message_id":"4"}',
                             )
     
     subject = MessageSenderMixin()
     
-    with pytest.raises(InvalidMessageNameException):
-        subject.send("transaction",test_payload,None)
+    subject.send("transaction",test_payload,None).should.throw(InvalidMessageNameException)
     
-    subject.send("test_event",test_payload,test_callback)
-    subject.response_callbacks["4"].should.equal("test_callback")
+    response = subject.send("test_event",test_payload,send_callback)
+    subject.response_callbacks["4"].should.equal(globals()["send_callback"])
     
+    response.should.equal({"message_id":"4"})
     
+@httpretty.activate
 def test_send_transaction():
     """
     MessageSenderMixin._send_transaction() Test plan:
         -ensure callback is set on success
         -ensure that a response is received
     """
-    pass
-
+    
+    test_payload = {"item":"shoe"}
+    
+    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/message",
+                            body='{"message_id":"4"}',
+                            )
+    
+    subject = MessageSenderMixin()
+    
+    response = subject.send("test_event",test_payload,send_callback)
+    subject.response_callbacks["4"].should.equal(globals()["send_callback"])
+    
+    response.should.equal({"message_id":"4"})
+    
+@httpretty.activate
 def test_poll_responses():
     """
     MessageSenderMixin._poll_responses() Test plan:
@@ -48,8 +64,31 @@ def test_poll_responses():
         -Ensure False returned if post_poll_responses hook returns false
         -Ensure a collection of responses returned on success
     """
-    pass
     
+    httpretty.register_uri(httpretty.GET,HPIT_URL_ROOT+"/responses",
+                            body='{"responses":"4"}',
+                            )
+    
+    
+    def returnFalse():
+        return False
+    def returnTrue():
+        return True
+    
+    test_message_sender_mixin = MessageSenderMixin()
+    
+    setattr(test_message_sender_mixin,"pre_poll_responses",returnFalse)
+    setattr(test_message_sender_mixin,"post_poll_responses",returnTrue)
+    test_message_sender_mixin._poll_responses().should.equal(False)
+    
+    setattr(test_message_sender_mixin,"pre_poll_responses",returnTrue)
+    setattr(test_message_sender_mixin,"post_poll_responses",returnFalse)
+    test_message_sender_mixin._poll_responses().should.equal(False)
+    
+    setattr(test_message_sender_mixin,"pre_poll_responses",returnTrue)
+    setattr(test_message_sender_mixin,"post_poll_responses",returnTrue)
+    test_message_sender_mixin._poll_responses().should.equal("4")
+
 def test_dispatch_responses():
     """
     MessageSenderMixin._dispatch_responses() Test plan:
@@ -61,4 +100,42 @@ def test_dispatch_responses():
         -Catch not callable error
         -Ensure true returned on completions
      """
-    pass
+    bad_response = [{"bad_response": "boo"}]
+    bad_response2 = [{"message":{"id":"4"}}]
+    good_response = [{"message": {"id":"4"},"response":"2"}]
+
+    
+    def returnFalse():
+        return False
+    def returnTrue():
+        return True
+    def callback1(payload):
+        return True
+    
+    test_message_sender_mixin = MessageSenderMixin()
+    
+    test_message_sender_mixin.response_callbacks["4"] = callback1
+    setattr(test_message_sender_mixin,"pre_dispatch_responses",returnFalse)
+    setattr(test_message_sender_mixin,"post_dispatch_responses",returnTrue)
+    test_message_sender_mixin._dispatch_responses(good_response).should.equal(False)
+    
+    setattr(test_message_sender_mixin,"pre_dispatch_responses",returnTrue)
+    setattr(test_message_sender_mixin,"post_dispatch_responses",returnFalse)
+    test_message_sender_mixin._dispatch_responses(good_response).should.equal(False)
+    
+    setattr(test_message_sender_mixin,"pre_dispatch_responses",returnTrue)
+    setattr(test_message_sender_mixin,"post_dispatch_responses",returnTrue)
+    
+    test_message_sender_mixin._dispatch_responses.when.called_with(bad_response).should.throw(ResponseDispatchError)
+    test_message_sender_mixin._dispatch_responses.when.called_with(bad_response2).should.throw(ResponseDispatchError)
+    
+    del test_message_sender_mixin.response_callbacks["4"]
+    test_message_sender_mixin._dispatch_responses.when.called_with(good_response).should.throw(ResponseDispatchError)
+    test_message_sender_mixin.response_callbacks["4"] = 5
+    test_message_sender_mixin._dispatch_responses.when.called_with(good_response).should.throw(ResponseDispatchError)
+    
+    test_message_sender_mixin.response_callbacks["4"] = callback1
+    test_message_sender_mixin._dispatch_responses(good_response).should.equal(True)
+    
+    
+
---
+++ tests/client/test_tutor.py
@@ -0,0 +1,44 @@
+import sure
+import pytest
+import httpretty
+import requests
+
+from client import Tutor
+from client.settings import HPIT_URL_ROOT
+from client.exceptions import ConnectionError
+
+@httpretty.activate
+def test_connect():
+    """
+    Tutor.test_connect() Test plan:
+        -ensure that true returned on good connection
+        -ensure that exception thrown on bad connection
+    """
+
+    test_tutor = Tutor("test_name",None)
+    
+    test_tutor.connect().should.throw.(ConnectionError)
+    
+    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/tutor/connect/",
+                            body='{"entity_name":"tutor_name","entity_id":"4"}',
+                            )
+    
+    test_tutor.connected.should.equal(False)
+    test_tutor.connect().should.equal(True)
+    test_tutor.connected.should.equal(True)
+    
+@httpretty.activate
+def test_disconnect():
+    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/tutor/disconnect",
+                            body='OK',
+                            )
+    
+    test_tutor = Tutor("test_name",None)
+    test_tutor.connected = True
+    
+    test_tutor.disconnect().should.equal(False)
+    test_tutor.connected.should.equal(False)
+    
+    
+
+
