25d2e80d2819507e4932c27d0a3c2d15d8854d73
Raymond Chandler III <raymondchandleriii@gmail.com>
2014-06-26 20:50:50 -0400

    The syncdb command is now working.

--- commands/syncdb.py
+++ commands/syncdb.py
@@ -1,4 +1,5 @@
-from server import db
+from server.app import ServerApp
+db = ServerApp.get_instance().db
 
 class Command:
     description = "Creates all the tables in the database."
@@ -6,8 +7,10 @@ class Command:
     def __init__(self, manager, parser):
         self.manager = manager
 
-    def run(self, args, configuration):
-        self.args = args
+    def run(self, arguments, configuration):
+        self.arguments = arguments
         self.configuration = configuration
 
         db.create_all()
+
+        print("DONE! - Sync'd the database with the data model")
