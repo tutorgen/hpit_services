3607b9480a9d69618c9a2f8244d3e241ee89a59f
bmsauer <brianmsauer@gmail.com>
2014-06-30 12:21:14 -0400

    Windows compat stuff, seems to work fine.

--- server_wrapper.py
+++ server_wrapper.py
@@ -4,7 +4,17 @@ is used to start the server, which is coded using relative imports.  This is a
 workaround so the relative imports are preserved in Windows.
 """
 
-from server import app
-from server.settings import settings
+#from server import app
+#from server.settings import settings
 
-app.run(port = int(settings.HPIT_BIND_PORT))
+from server.settings import ServerSettingsManager
+settings_manager = ServerSettingsManager.init_instance('production')
+
+from server.views.api import *
+from server.views.dashboard import *
+from server.app import ServerApp
+
+server_app = ServerApp.get_instance()
+server_app.bootstrap_user()
+
+server_app.app.run(port = int(settings_manager.get_instance().settings.HPIT_BIND_PORT))
