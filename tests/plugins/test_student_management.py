6f98ae700f08233f98ef9b311153cd1ba76730a8
bmsauer <brianmsauer@gmail.com>
2014-07-07 10:30:47 -0400

    Added student management test, removed redundant code in skill management test

--- tests/plugins/test_student_management.py
+++ tests/plugins/test_student_management.py
@@ -9,7 +9,7 @@ from client import Plugin
 from client.settings import HPIT_URL_ROOT
 from pymongo import MongoClient
 
-from plugins import SkillManagementPlugin
+from plugins import StudentManagementPlugin
 
 test_subject = None
 
@@ -21,7 +21,7 @@ def setup_function(function):
     
     global test_subject
     
-    test_subject = SkillManagementPlugin(123,456,None)
+    test_subject = StudentManagementPlugin(123,456,None)
     
     httpretty.enable()
     httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/plugin/connect/test_name",
@@ -45,7 +45,7 @@ def teardown_function(function):
     pass
 def test_constructor():
     """
-    SkillManagementPlugin.__init__() Test plan:
+    StudentManagementPlugin.__init__() Test plan:
         -ensure name, logger set as parameters
         -ensure that mongo is an instance of mongo client
     """
@@ -53,39 +53,39 @@ def test_constructor():
     isinstance(test_subject.mongo,MongoClient).should.equal(True)
     
     pass
-def test_add_skill_callback():
+def test_add_student_callback():
     """
-    SkillManagementPlugin.add_skill_callback() Test plan:
+    StudentManagementPlugin.add_student_callback() Test plan:
         -Mock logger, ensure written to when called
     """
     test_message = "This is a message"
-    calls = [call("ADD_SKILL"),call(test_message)]
+    calls = [call("ADD_STUDENT"),call(test_message)]
     mock=MagicMock()
     test_subject.logger = mock
-    test_subject.add_skill_callback(test_message)
+    test_subject.add_student_callback(test_message)
     mock.debug.assert_has_calls(calls)
     
-def test_remove_skill_callback():
+def test_remove_student_callback():
     """
-    SkillManagementPlugin.remove_skill_callback() Test plan:
+    StudentManagementPlugin.remove_student_callback() Test plan:
         -Mock logger, ensure written to when called
     """
     test_message = "This is a message"
-    calls = [call("REMOVE_SKILL"),call(test_message)]
+    calls = [call("REMOVE_STUDENT"),call(test_message)]
     mock=MagicMock()
     test_subject.logger = mock
-    test_subject.remove_skill_callback(test_message)
+    test_subject.remove_student_callback(test_message)
     mock.debug.assert_has_calls(calls)
  
-def test_get_skill_callback():
+def test_get_student_callback():
     """
-    SkillManagementPlugin.get_skill_callback() Test plan:
+    StudentManagementPlugin.get_skill_callback() Test plan:
         -Mock logger, ensure written to when called
     """
     test_message = "This is a message"
-    calls = [call("GET_SKILL"),call(test_message)]
+    calls = [call("GET_STUDENT"),call(test_message)]
     mock=MagicMock()
     test_subject.logger = mock
-    test_subject.get_skill_callback(test_message)
+    test_subject.get_student_callback(test_message)
     mock.debug.assert_has_calls(calls)
     pass
