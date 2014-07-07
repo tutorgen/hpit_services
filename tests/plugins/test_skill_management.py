61bb26ae82eb536dfbeda3ebc2f3f4f2b0864b1e
bmsauer <brianmsauer@gmail.com>
2014-07-07 10:25:22 -0400

    finished skill_management plugin tests.

--- tests/plugins/test_skill_management.py
+++ tests/plugins/test_skill_management.py
@@ -18,84 +18,80 @@ def setup_function(function):
     class.  setup_method is invoked for every test method of a class.
     """
     
-    """
+    
     global test_subject
     
+    test_subject = SkillManagementPlugin(123,456,None)
     
     httpretty.enable()
     httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/plugin/connect/test_name",
                             body='{"entity_name":"skill_management_plugin","entity_id":"4"}',
                             )
-    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/plugin/test_name/subscribe/transaction",
+
+    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/plugin/subscribe/",
                             body='',
                             )
-    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/plugin/test_name/subscribe/add_skill",
+    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/plugin/subscribe/",
                             body='',
                             )
-    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/plugin/test_name/subscribe/remove_skill",
+    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/plugin/subscribe/",
                             body='',
                             )
-    httpretty.register_uri(httpretty.POST,HPIT_URL_ROOT+"/plugin/test_name/subscribe/get_skill",
-                            body='',
-                            )
-    test_subject = SkillManagementPlugin("test_name",None)
-    """
-    pass
+    
+   
 def teardown_function(function):
     """ teardown any state that was previously setup with a setup_method
     call.
     """
-    #global test_subject
+    global test_subject
     
-    #httpretty.disable()
-    #httpretty.reset()
-    #test_subject = None
+    httpretty.disable()
+    httpretty.reset()
+    test_subject = None
     pass
 def test_constructor():
     """
     SkillManagementPlugin.__init__() Test plan:
         -ensure name, logger set as parameters
-        -ensure callbacks are set
+        -ensure that mongo is an instance of mongo client
     """
-    #test_subject.name.should.equal("test_name")
-    #test_subject.logger.should.equal(None)
-    #test_subject.callbacks["add_skill"].should.equal(test_subject.test_plugin_callback)
-    #test_subject.callbacks["remove_skill"].should.equal(test_subject.example_plugin_callback)
-    #test_subject.callbacks["get_skill"].should.equal(test_subject.test_plugin_callback)
+    test_subject.logger.should.equal(None)
+    isinstance(test_subject.mongo,MongoClient).should.equal(True)
+    
     pass
 def test_add_skill_callback():
     """
     SkillManagementPlugin.add_skill_callback() Test plan:
         -Mock logger, ensure written to when called
     """
-    #test_message = "This is a message"
-    #calls = [call("ADD_SKILL"),call(test_message)]
-    #mock=MagicMock()
-    #test_subject.logger = mock
-    #test_subject.add_skill_callback(test_message)
-    #mock.debug.assert_has_calls(calls)
-    pass
+    test_message = "This is a message"
+    calls = [call("ADD_SKILL"),call(test_message)]
+    mock=MagicMock()
+    test_subject.logger = mock
+    test_subject.add_skill_callback(test_message)
+    mock.debug.assert_has_calls(calls)
+    
 def test_remove_skill_callback():
     """
     SkillManagementPlugin.remove_skill_callback() Test plan:
         -Mock logger, ensure written to when called
     """
-    #test_message = "This is a message"
-    #calls = [call("REMOVE_SKILL"),call(test_message)]
-    #mock=MagicMock()
-    #test_subject.logger = mock
-    #test_subject.add_skill_callback(test_message)
-    #mock.debug.assert_has_calls(calls)
-    pass
+    test_message = "This is a message"
+    calls = [call("REMOVE_SKILL"),call(test_message)]
+    mock=MagicMock()
+    test_subject.logger = mock
+    test_subject.remove_skill_callback(test_message)
+    mock.debug.assert_has_calls(calls)
+ 
 def test_get_skill_callback():
     """
     SkillManagementPlugin.get_skill_callback() Test plan:
         -Mock logger, ensure written to when called
     """
-    #test_message = "This is a message"
-    #calls = [call("GET_SKILL"),call(test_message)]
-    #mock=MagicMock()
-    #test_subject.logger = mock
-    #test_subject.add_skill_callback(test_message)
-    #mock.debug.assert_has_calls(calls)
+    test_message = "This is a message"
+    calls = [call("GET_SKILL"),call(test_message)]
+    mock=MagicMock()
+    test_subject.logger = mock
+    test_subject.get_skill_callback(test_message)
+    mock.debug.assert_has_calls(calls)
     pass
