c11609ea369224d9f1bbd90083178e887242e6d1
bmsauer <brianmsauer@gmail.com>
2014-07-09 15:43:00 -0400

    Callbacks, seven states

--- tutors/hint_factory_base_tutor.py
+++ tutors/hint_factory_base_tutor.py
@@ -1,8 +1,10 @@
 import logging
 import json
 from json import JSONEncoder, JSONDecoder
+import time
 
-from client import Tutor      
+from client import Tutor
+from client.exceptions import ConnectionError
 
 class HintFactoryBaseTutor(Tutor):
     
@@ -19,17 +21,22 @@ class HintFactoryBaseTutor(Tutor):
         self.hint_factory_state_encoder = HintFactoryStateEncoder()
             
     def post_state(self,hint_factory_state):
-        self.send("hf_push_state",{"state":hint_factory_state_encoder.encode(hint_factory_state)},self.post_state_callback)
+        #self.send("hf_push_state",{"state":hint_factory_state_encoder.encode(hint_factory_state)},self.post_state_callback)
+        self.post_state_callback({"status":"OK"})
         
     def hint_exists(self,hint_factory_state):
-        self.send("hf_hint_exists",{"state":hint_factory_state_encoder.encode(hint_factory_state)},self.hint_exists_callback)
-        
+        #self.send("hf_hint_exists",{"state":hint_factory_state_encoder.encode(hint_factory_state)},self.hint_exists_callback)
+        self.hint_exists_callback({"status":"OK","exists":"YES"})
+    
     def get_hint(self,hint_factory_state):
-        self.send("hf_get_hint",{"state":hint_factory_state_encoder.encode(hint_factory_state)},self.get_hint_callback)
-        
+        #self.send("hf_get_hint",{"state":hint_factory_state_encoder.encode(hint_factory_state)},self.get_hint_callback)
+        time.sleep(3)
+        self.get_hint_callback({"status":"OK","exists":"YES","hint_text":"This is a hint."})
+    
     def init_problem(self,hint_factory_start_state, hint_factory_goal_state):
-        self.send("hf_get_hint",{"start_state":hint_factory_start_state,"goal_problem":goal_problem_string},self.init_problem_callback)
-        
+        #self.send("hf_get_hint",{"start_state":hint_factory_start_state,"goal_problem":goal_problem_string},self.init_problem_callback)
+        self.init_problem_callback({"status":"OK"})
+    
     def main_callback(self):
         raise NotImplementedError("Please implement a main_callback for your tutor.")
         
@@ -77,6 +84,11 @@ class HintFactoryState(object):
     def append_step(self,step):
         self.steps.append(step)
         
+        
+class BadHintFactoryResponseError(Exception):
+    """
+    Raised when a response is invalid or bad.
+    """
  
  
 #------------------------------------------------------------------------------
@@ -104,31 +116,101 @@ class HintFactoryTutor(HintFactoryBaseTutor):
         super().__init__(entity_id,api_key,logger,run_once,args)
         
         self.game_states = {
-            "2x + 4 = 12" : HintFactoryTutor.GameState("2x + 4 = 12", [("Subtract 4","2x = 8"),("Subtract 12","2x + 4 - 12 = 0"),("Divide 2","(2x + 4) / 2 = 6")]),
+            "2x + 4 = 12" : HintFactoryTutor.GameState("2x + 4 = 12", [("Subtract 4","2x = 8"),("Subtract 12","2x - 8 = 0"),("Divide 2","(2x + 4) / 2 = 6")]),
             "2x = 8" : HintFactoryTutor.GameState("2x = 8",[("Add 4","2x + 4 = 12"),("Subtract 8","2x - 8 = 0"),("Divide 2","x = 4")]),
+            "2x - 8 = 0" : HintFactoryTutor.GameState("2x - 8 = 0",[("Divide 2","(2x + 8) / 2 = 0"),("Add 8","2x = 8")]),
+            "(2x + 8) / 2 = 0" : HintFactoryTutor.GameState("(2x + 8) / 2 = 0",[("Multiply 2","2x - 8 = 0")]),   #-8 error
+            "x = 4" : HintFactoryTutor.GameState("x = 4",[]),
+            "(2x + 4) / 2 = 6" : HintFactoryTutor.GameState("(2x + 4) / 2 = 6",[("Multiply 2","2x + 4 = 12"),("Subtract 6","(2x + 4) / 2 - 6 = 0")]),   #-4 error
+            "(2x + 4) / 2 - 6 = 0" : HintFactoryTutor.GameState("(2x + 4) / 2 - 6 = 0",[("Multiply 2","2x - 8 = 0"),("Add 6","(2x + 4) / 2 = 6")]),
         }
         
         self.cur_state = self.game_states["2x + 4 = 12"]
         self.goal = "x = 4"
+        self.hint = None
+        self.waiting_for_hint = False
+        self.exists = False
         
-    def main_callback(self):
+    def post_connect(self):
+        self.init_problem("2x + 4 = 12","x = 4")
+        
+    def init_problem_callback(self,response):
+        try:
+            if response['status'] != "OK":
+                raise ConnectionError("Failure to post state to HPIT server.")
+        except KeyError as e:
+            print("The response does not contain a status code. "+ str(type(e))+str(e.args))
+        
+    def post_state_callback(self,response):
+        try:
+            if response['status'] != "OK":
+                raise ConnectionError("Failure to post state to HPIT server.")
+        except KeyError as e:
+            print("The response does not contain a status code. "+ str(type(e))+str(e.args))
+            
+    def hint_exists_callback(self,response):
+        try:
+            if response['status'] != "OK":
+                raise ConnectionError("Failure to post state to HPIT server.")
+        except KeyError as e:
+            print("The response does not contain a status code. "+ str(type(e))+str(e.args))
+            
+        if response['exists'] == "YES":
+            self.exists = True
+        elif response['exists'] == "NO":
+            self.exists = False
+            
+    def get_hint_callback(self,response):
+        try:
+            if response['status'] != "OK":
+                raise ConnectionError("Failure to post state to HPIT server.")
 
-        print(str(self.cur_state))
-        choice = input("Operation: (0 to quit, h for hint) ")
-        
-        if choice == "0":
-            return False
-        elif choice == "h":
-            print ("Hint")
-        else:
-            self.cur_state = self.game_states[self.cur_state.possible_transitions[int(choice)-1][1]]
-        
-        print ("=======================================")
+            if response['exists'] == "YES":
+                self.hint = response["hint_text"]
+            elif response['exists'] == "NO":
+                self.hint = "No hint available."
+                
+        except KeyError as e:
+            print("The response does not contain the proper fields. "+ str(type(e))+str(e.args))
+    
+    def main_callback(self):
         
         if self.cur_state.problem == self.goal:
+            print(self.goal)
             print("You solved the problem!")
-            return false
+            return False
+        
+        if self.waiting_for_hint == True:
+            if self.hint!=None:
+                print(self.hint)
+                self.hint = None
+                self.waiting_for_hint = False
+            else:
+                return True
+        
+        else:
+            self.hint_exists(self.cur_state)
+            time.sleep(1)
+            self.post_state(self.cur_state)
+    
+            print(str(self.cur_state))
+            if self.exists == True:
+                print("**Hint exists**")
+                
+            choice = input("Operation: (0 to quit, h for hint) ")
             
+            if choice == "0":
+                return False
+            elif choice == "h":
+                self.waiting_for_hint = True
+                print("Waiting for hint...")
+                self.get_hint(self.cur_state)
+                return True
+            else:
+                self.cur_state = self.game_states[self.cur_state.possible_transitions[int(choice)-1][1]]
+                self.exists = False
+                
+            print ("=======================================")
         
         return True
         
