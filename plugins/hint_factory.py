80f95a4fa1aff75b025cd1b531eed05bde6dbed5
Raymond Chandler III <raymondchandleriii@gmail.com>
2014-07-09 15:21:27 -0400

    WIP ray

--- plugins/hint_factory.py
+++ plugins/hint_factory.py
@@ -1,22 +1,174 @@
 # from client import Plugin
+import hashlib
+from itertools import groupby
 
 from bulbs.model import Node, Relationship
 from bulbs.neo4jserver import Graph
 from bulbs import property as prop
 from bulbs.utils import current_datetime
 
-class MDPProblemNode(Node):
+class StateFinderMixin:
+
+    def hash_string(self, string):
+        return hashlib.sha256(bytes(string.encode('utf-8'))).hexdigest()
+
+
+    def push_state(self, graph, action_string, problem_string):
+        """
+            graph - the graph that this node is part of
+            action_string - a string representation of the action taken
+            problem_string - a string representation of the problem at it's current state
+        """
+        #Check if this state exists directly off this node
+        problem_hash = self.hash_string(problem_string)
+        action_hash = self.hash_string(action_string)
+
+        #Does a direct decendent match the state and action?
+        state, action = self.direct_decendent_matches_state_action(problem_hash, action_hash)
+
+        if state:
+            state.store_count = state.store_count + 1
+            state.save()
+            self.update_action_probabilites(action)
+            return state
+
+        #Does a direct decendent match the state but not the action?
+        state = self.direct_decendent_matches_state(problem_hash)
+
+        if state:
+            new_action = graph.actions.create(self, state)
+            new_action.action_string = action_string
+            new_action.action_hash = action_hash
+            new_action.save()
+            self.update_action_probabilites(new_action)
+            return state
+
+        #No decendent state - Add new state and action to connect it.
+        new_state = graph.states.create(state_string=problem_string, state_hash=problem_hash)
+
+        new_action = graph.actions.create(self, new_state)
+        new_action.action_string = action_string
+        new_action.action_hash = action_hash
+        new_action.save()
+
+        self.update_action_probabilites(new_action)
+
+        return new_state
+
+
+    def direct_decendent_matches_state_action(self, state_hash, action_hash):
+        edges = self.outE()
+
+        for e in edges:
+            if e.action_hash == action_hash:
+                node = e.outV()
+                if node.state_hash == state_hash:
+                    return (node, e)
+
+        return None
+
+
+    def direct_decendent_matches_state(self, state_hash):
+        nodes = self.outV()
+
+        for node in nodes:
+            if node.state_hash == state_hash:
+                return node
+
+        return None
+
+        
+    def update_action_probabilites(self, action):
+        siblings = [(s.outV().store_count, s) for s in action.inV().outE() if s.action_hash == action.action_hash]
+        total_store_count = sum(map(lambda s: s[0], siblings))
+
+        if total_store_count <= 0:
+            total_store_count = 1
+
+        for store_count, s in siblings:
+            s.probability = store_count / total_store_count
+            s.save()
+
+
+    #We could speed this up by doing a probabilistic search.
+    #For now it is a naive bredth first search. - Ray
+    def find_state_by_hash(self, state_hash, visited=None):
+        search_states = set(self.outV())
+
+        if not visited:
+            visited = set()
+
+        search_states = search_states - visited
+
+        #Breadth first
+        for s in search_states:
+            if s.state_hash == state_hash:
+                return s
+
+        #Don't search me again
+        visited = search_states + visited
+
+        for s in search_states:
+            found = s.find_state_by_hash(state_hash, visited)
+
+            if found:
+                return found
+
+        return None
+
+
+    def find_state_by_string(self, state_string):
+        return self.find_state_by_hash(self.hash_string(state_string))
+
+
+class MDPProblemNode(Node, StateFinderMixin):
     element_type = "mdp_problem"
 
     created = prop.DateTime(default=current_datetime, nullable=False)
     start_text = prop.String()
     goal_text = prop.String()
+    discount_factor = prop.Float(default=0.5)
+
+    def find_goal_state(self):
+        self.find_state_by_string(self.goal_text)
 
 
-class MDPStateNode(Node):
+    def _do_bellman(self, state, goal_hash):
+        GOAL_REWARD = 100
+        STD_REWARD = 10
+
+        if state.state_hash == goal_hash:
+            state.bellman_value = GOAL_REWARD
+        else:
+            child_edges = state.outE()
+
+            action_values = []
+            for action, edges in groupby(child_edges, lambda x: x.action_hash):
+                bellman_sum = sum([e.probability * self.discount_factor * self._do_bellman(e.outV(), goal_hash) for e in edges])
+                action_values.append(STD_REWARD + bellman_sum)
+
+            state.bellman_value = max(action_values)
+
+        state.save()
+        return state.bellman_value
+
+
+    def do_bellman_update(self):
+        """
+        Perform Bellman value iteration upon the whole graph to assign
+        reward values.
+        """
+
+        self._do_bellman(self, self.hash_string(self.goal_text))
+
+
+class MDPStateNode(Node, StateFinderMixin):
     element_type = "mdp_state"
 
     created = prop.DateTime(default=current_datetime, nullable=False)
+    state_hash = prop.String()
+    state_string = prop.String()
+    store_count = prop.Integer(default=1)
     reward = prop.Float(default=0.0)
     bellman_value = prop.Float(default=0.0)
 
@@ -25,6 +177,8 @@ class MDPAction(Relationship):
     label = "action"
 
     created = prop.DateTime(default=current_datetime, nullable=False)
+    action_hash = prop.String()
+    action_string = prop.String()
     probability = prop.Float(default=0.0)
 
 
@@ -55,13 +209,6 @@ class MasterGraph(Graph):
         #Only one should exists
         return next(nodes) if nodes else None
 
-
-    def perform_value_iterator(self, problem):
-        pass
-
-
-    def add_problem_state(self, problem, old_state, new_state):
-        pass
 
 
 # class HintFactoryPlugin(Plugin):
@@ -107,5 +254,9 @@ if __name__ == '__main__':
     db = MasterGraph()
     import pdb; pdb.set_trace()
     problem = db.create_problem('4x-6=10', 'x=4')
+    state = problem.push_state(db, 'addition', '4x-6+6=10+6')
+    state = state.push_state(db, 'simplification', '4x=16')
+    state = state.push_state(db, 'division', '4x/4=16/4')
+    state = state.push_state(db, 'simplification', 'x=4')
     x = 5
 
