45332b0efa866d1a11eeeedba50e367e231a3c86
bmsauer <brianmsauer@gmail.com>
2014-07-24 12:30:30 -0400

    Replay tutor test, fixed bugs in replay tutor, added duck punching httpretty to commands.

--- tutors/replay.py
+++ tutors/replay.py
@@ -15,6 +15,16 @@ from hpitclient import Tutor
 
 class ReplayTutor(Tutor):
     def __init__(self, entity_id, api_key, logger, run_once=None, args=None):
+        """
+        Constructor: takes entity, api_key, logger, and run_once, like other tutors,
+        but in addition, a JSON string of arguments.  These arguments are:
+            beforeTime: a date in the form of 02-11-2020 14:03:09 that indicates only messages
+                should be replayed that happened before this date.
+            afterTime:  like before time, but specifies the date that only messages created after
+                should be replayed
+            filter:  (required) a JSON object that serves as the custom filter for the MongoDB messages
+            db_name: (required)  The name of the database to look in
+        """
         super().__init__(entity_id, api_key, self.main_callback, run_once=run_once)
         self.run_once = run_once
         self.logger = logger
@@ -22,35 +32,49 @@ class ReplayTutor(Tutor):
         if args: 
             self.args = json.loads(args[1:-1])
         else:
-            self.args = None
+            raise NoArgumentsException("A JSON string of arguments should be passed to the tutor.")
+        
+        try:
+            self.db_name = self.args["db_name"]
+        except KeyError:
+            raise InvalidArgumentsException("Args need to at least contain db_name")
+            
+        try:
+            filter = self.args["filter"]
+        except KeyError:
+            raise InvalidArgumentsException("Args need to at least contain filter")
         
     def main_callback(self):
         client = MongoClient()
-        messages = client.hpit_development.messages
-        self.logger.debug("filter is: " + str(self.args["filter"]))
+        messages = client[self.db_name].messages
+        if self.logger:
+            self.logger.debug("filter is: " + str(self.args["filter"]))
+
         for message in messages.find(self.args["filter"]):
             
             criteriaFlag = 0
             successFlag = 0
-            documentTime = ObjectId(message["_id"]).generation_time
+            documentTime = datetime.strptime(message["time_created"], "%m-%d-%Y %H:%M:%S")
             documentTime = documentTime.replace(tzinfo=pytz.utc)
+            
             if "afterTime" in self.args:
                 criteriaFlag = criteriaFlag | 1 
                 afterTime  = datetime.strptime(self.args["afterTime"], "%m-%d-%Y %H:%M:%S")
                 afterTime = afterTime.replace(tzinfo=pytz.utc)
                 if afterTime <= documentTime:
-                    successFlag  = criteriaFlag | 1
+                    successFlag  = successFlag | 1
                     
             if "beforeTime" in self.args:
                 criteriaFlag = criteriaFlag | 2
                 beforeTime = datetime.strptime(self.args["beforeTime"], "%m-%d-%Y %H:%M:%S")
                 beforeTime = beforeTime.replace(tzinfo=pytz.utc)
                 if beforeTime >= documentTime:
-                    successFlag = criteriaFlag | 2
-                    
+                    successFlag = successFlag | 2
+            
             if criteriaFlag == successFlag:
                 self.send(message["event"],message["payload"])
-                self.logger.debug("REPLAYED: "+str(message))
+                if self.logger:
+                    self.logger.debug("REPLAYED: "+str(message))
             
         
         if self.run_once:
