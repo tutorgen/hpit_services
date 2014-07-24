c448c538d37de7f65546444a66911f0a5d31571d
bmsauer <brianmsauer@gmail.com>
2014-07-24 12:41:42 -0400

    Test scaffold

---
+++ tests/plugins/test_data_connector_plugin.py
@@ -0,0 +1,50 @@
+
+from plugins import DataShopConnectorPlugin
+
+class TestDataShopConnectorPlugin(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    
+    def test_constructor(self):
+        """
+        DataShopConnectorPlugin.__init__() Test plan:
+        """
+        pass
+    
+    def test_get_dataset_metadata(self):
+        """
+        DataShopConnectorPlugin.get_dataset_metadata() Test plan:
+        """
+        pass
+    
+    def test_get_sample_metadata(self):
+        """
+        DataShopConnectorPlugin.get_sample_metadata() Test plan:
+        """
+        pass
+    
+    def test_get_transactions(self):
+        """
+        DataShopConnectorPlugin.get_transactions() Test plan:
+        """
+        pass
+    
+    def test_get_student_steps(self):
+        """
+        DataShopConnectorPlugin.get_student_steps() Test plan:
+        """
+        pass
+    
+    def test_add_custom_field(self):
+        """
+        DataShopConnectorPlugin.add_custom_field() Test plan:
+        """
+        pass
+    
+    def test_datashop_request(self):
+        """
+        DataShopConnectorPlugin.datashop_request() Test plan:
+        """
+        pass
