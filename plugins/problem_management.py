0a1ddbf0665cdb5fb4209de8594dacb2490150a0
bmsauer <brianmsauer@gmail.com>
2014-08-11 16:48:31 -0400

    Remove problem, edit problem, get problem.

--- plugins/problem_management.py
+++ plugins/problem_management.py
@@ -1,6 +1,9 @@
 from hpitclient import Plugin
 
+from datetime import datetime
+
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 class ProblemManagementPlugin(Plugin):
 
@@ -8,8 +11,9 @@ class ProblemManagementPlugin(Plugin):
         super().__init__(entity_id, api_key)
         self.logger = logger
         self.mongo = MongoClient('mongodb://localhost:27017/')
-        self.db = self.mongo.hpit.problems
-
+        self.db = self.mongo.hpit.hpit_problems
+        
+        self.problem_fields = ["problem_name","problem_text"]
 
     def post_connect(self):
         super().post_connect()
@@ -27,98 +31,118 @@ class ProblemManagementPlugin(Plugin):
         problem_text = message['problem_text']
 
         problem = self.db.find_one({
-            'sender_entity_id': sender_entity_id,
             'problem_name': problem_name,
+            'problem_text': problem_text,
             })
 
         if not problem:
-            self.db.insert({
-                'sender_entity_id': sender_entity_id,
+            problem_id = self.db.insert({
+                'edit_allowed_id': sender_entity_id,
                 'problem_name': problem_name,
                 'problem_text': problem_text,
+                'date_created': datetime.now(),
             })
 
             self.send_response(message['message_id'], {
                 'problem_name': problem_name,
                 'problem_text': problem_text,
                 'success': True,
-                'updated': False
+                'problem_id':problem_id
             })
+        
         else:
-            self.db.update({'_id': problem['_id']}, {
-                'sender_entity_id': sender_entity_id,
-                'problem_name': problem_name,
-                'problem_text': problem_text,
-            })
-
             self.send_response(message['message_id'], {
-                'problem_name': problem_name,
-                'problem_text': problem_text,
-                'success': True,
-                'updated': True
+                'error': "This problem already exists.  Try cloning the 'problem_id' sent in this response.",
+                'problem_id': problem["_id"],
             })
-
+        
 
     def remove_problem_callback(self, message):
         sender_entity_id = message['sender_entity_id']
-        problem_name = message['problem_name']
+        problem_id = message['problem_id']
 
         problem = self.db.find_one({
-            'sender_entity_id': sender_entity_id,
-            'problem_name': problem_name,
+            'edit_allowed_id': sender_entity_id,
+            'problem_name': problem_id,
         })
 
         if problem:
             self.db.remove({
-                'sender_entity_id': sender_entity_id,
-                'problem_name': problem_name,
+                'edit_allowed_id': sender_entity_id,
+                '_id': ObjectID(problem_id),
             })
 
             self.send_response(message['message_id'], {
-                'problem_name': problem_name,
+                'problem_id': problem_id,
                 'exists': True,
                 'success': True,
             })
         else:
             self.send_response(message['message_id'], {
-                'problem_name': problem_name,
+                "error": "Could not delete problem; it does not exist",
+                'problem_id': problem_id,
                 'exists': False,
                 'success': False
             })
 
 
     def get_problem_callback(self, message):
-        sender_entity_id = message['sender_entity_id']
-        problem_name = message['problem_name']
+        problem_id = message['problem_id']
 
         problem = self.db.find_one({
-            'sender_entity_id': sender_entity_id,
-            'problem_name': problem_name,
+            '_id': ObjectID(problem_id),
         })
 
         if not problem:
             self.send_response(message['message_id'], {
-                'problem_name': problem_name,
+                'error': "Error:  problem with id" + problem_id + " does not exist.",
                 'exists': False,
                 'success': False
             })
         else:
             self.send_response(message['message_id'], {
-                'problem_name': problem_name,
+                'problem_name': problem["problem_name"],
                 'problem_text': problem['problem_text'],
+                'date_created': problem["date_created"],
+                'edit_allowed_id' : problem["edit_allowed_id"],
                 'exists': True,
                 'success': True
             })
-
-    def list_problems_callback(self, message):
-        sender_entity_id = message['sender_entity_id']
-
-        problems = self.db.find({
-            'sender_entity_id': sender_entity_id
+    
+    def edit_problem_callback(self,message):
+        fields = message["fields"]
+        problem_id = message["problem_id"]
+        
+        problem = self.db.find_one({
+                "_id" : ObjectID(problem_id),
+                "edit_allowed_id" : message["sender_entity_id"],
         })
-
+        
+        if not problem:
+            self.send_response(message["message_id"], {
+                "error": "Error: either problem with provided id doesn't exist, or you do not have permission to edit.",
+                "success":'false',
+            })
+        else:
+            update_dict = {}
+            for f in self.problem_fields:
+                if f in fields:
+                    update_dict[f] = fields[f]
+                    problem[f] = fields[f]
+            
+            self.db.update({"_id":problem["_id"]},{"$set":update_dict})
+            self.send_response(message['message_id'], {
+                'problem_name': problem["problem_name"],
+                'problem_text': problem['problem_text'],
+                'date_created': problem["date_created"],
+                'edit_allowed_id' : problem["edit_allowed_id"],
+                'success': True
+            })
+    
+    
+    def list_problems_callback(self, message):
+        problems = self.db.find({})
         problems = [p for p in problems]
-
         self.send_response(message['message_id'], {
             'problems': problems,
             'success': True
--- tests/plugins/test_problem_management.py
+++ tests/plugins/test_problem_management.py
@@ -1,1 +1,25 @@
+import unittest
+from mock import *
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from bson.objectid import ObjectId
 
+from plugins import ProblemManagementPlugin
+
+class TestProblemManagementPlugin(unittest.TestCase):
+
+    def setUp(self):
+        """ setup any state tied to the execution of the given method in a
+        class.  setup_method is invoked for every test method of a class.
+        """
+        self.test_subject = ProblemManagementPlugin(123,456,None)
+        self.test_subject.db = self.test_subject.mongo.test_hpit.hpit_problems
+        
+    def tearDown(self):
+        """ teardown any state that was previously setup with a setup_method
+        call.
+        """
+        client = MongoClient()
+        client.drop_database("test_hpit")
+        
+        self.test_subject = None
