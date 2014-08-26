be6d9d5644cc6ef61ee979782d6cf9367a79dd7e
bmsauer <brianmsauer@gmail.com>
2014-08-26 12:43:07 -0400

    first commit

--- plugins/skill_management.py
+++ plugins/skill_management.py
@@ -2,30 +2,29 @@ from hpitclient import Plugin
 
 from pymongo import MongoClient
 
+from couchbase import Couchbase
+
 class SkillManagementPlugin(Plugin):
     def __init__(self, entity_id, api_key, logger, args = None):
         super().__init__(entity_id, api_key)
         self.logger = logger
-        #self.mongo = MongoClient('mongodb://localhost:27017/')
-        #self.db = self.mongo.hpit_skills
+        self.mongo = MongoClient('mongodb://localhost:27017/')
+        self.db = self.mongo.hpit.hpit_skills
+        
+        self.cache = Couchbase.connect(bucket = "default", host = "127.0.0.1")
 
     def post_connect(self):
         super().post_connect()
         
         self.subscribe(
-            add_skill=self.add_skill_callback,
-            remove_skill=self.remove_skill_callback,
-            get_skill=self.get_skill_callback)
+            get_skill_name=self.get_skill_name_callback,
+            get_skill_id = self.get_skill_id_callback)
 
     #Skill Management Plugin
-    def add_skill_callback(self, message):
-        self.logger.debug("ADD_SKILL")
+    def get_skill_name_callback(self, message):
+        self.logger.debug("GET_NAME")
         self.logger.debug(message)
 
-    def remove_skill_callback(self, message):
-        self.logger.debug("REMOVE_SKILL")
-        self.logger.debug(message)
-
-    def get_skill_callback(self, message):
-        self.logger.debug("GET_SKILL")
+    def get_skill_id_callback(self, message):
+        self.logger.debug("GET_ID")
         self.logger.debug(message)
--- tests/plugins/test_skill_management.py
+++ tests/plugins/test_skill_management.py
@@ -3,9 +3,8 @@ import responses
 import unittest
 from mock import *
 
-from hpitclient.settings import HpitClientSettings
-
-HPIT_URL_ROOT = HpitClientSettings.settings().HPIT_URL_ROOT
+from pymongo import MongoClient
+from pymongo import Collection
 
 from plugins import SkillManagementPlugin
 
@@ -16,11 +15,15 @@ class TestSkillManagementPlugin(unittest.TestCase):
         class.  setup_method is invoked for every test method of a class.
         """
         self.test_subject = SkillManagementPlugin(123,456,None)
+        self.test_subject.db = self.test_subject.mongo.test_hpit.hpit_skills
        
     def tearDown(self):
         """ teardown any state that was previously setup with a setup_method
         call.
         """
+        c = Collection()
+        c.drop_database("test_hpit")
+        
         self.test_subject = None
 
 
@@ -29,44 +32,29 @@ class TestSkillManagementPlugin(unittest.TestCase):
         SkillManagementPlugin.__init__() Test plan:
             -ensure name, logger set as parameters
             -ensure that mongo is an instance of mongo client
+            -ensure that a cache db is set up
         """
-        self.test_subject.logger.should.equal(None)
+        test_subject = SkillManagementPlugin(123,456,None)
+        test_subject.logger.should.equal(None)
+        
+        isinstance(
 
 
-    def test_add_skill_callback(self):
+    def test_get_skill_name_callback(self):
         """
-        SkillManagementPlugin.add_skill_callback() Test plan:
-            -Mock logger, ensure written to when called
+        SkillManagementPlugin.get_skill_name_callback() Test plan:
+            - pass in message without id, should respond with error
+            - pass in message with bogus id, should respond with error
+            - pass in message with good id, should respond with name
         """
-        test_message = "This is a message"
-        calls = [call("ADD_SKILL"),call(test_message)]
-        mock=MagicMock()
-        self.test_subject.logger = mock
-        self.test_subject.add_skill_callback(test_message)
-        mock.debug.assert_has_calls(calls)
-       
-
-    def test_remove_skill_callback(self):
+        pass
+    
+    def test_get_skill_id_callback(self):
         """
-        SkillManagementPlugin.remove_skill_callback() Test plan:
-            -Mock logger, ensure written to when called
+        SkillManagementPlugin.get_skill_id_callback() Test plan:
+            - pass in message without name, should respond with error
+            - pass in message with name for non existent, should create one
+            - pass in message with existing name, should return proper id
         """
-        test_message = "This is a message"
-        calls = [call("REMOVE_SKILL"),call(test_message)]
-        mock=MagicMock()
-        self.test_subject.logger = mock
-        self.test_subject.remove_skill_callback(test_message)
-        mock.debug.assert_has_calls(calls)
-
-     
-    def test_get_skill_callback(self):
-        """
-        SkillManagementPlugin.get_skill_callback() Test plan:
-            -Mock logger, ensure written to when called
-        """
-        test_message = "This is a message"
-        calls = [call("GET_SKILL"),call(test_message)]
-        mock=MagicMock()
-        self.test_subject.logger = mock
-        self.test_subject.get_skill_callback(test_message)
-        mock.debug.assert_has_calls(calls)
+        pass
+        
