c71c05264da468f467fa1a0771d88a248eff6e1e
Raymond Chandler III <raymondchandleriii@gmail.com>
2014-08-26 18:41:39 -0400

    Removed PDB statement.

--- plugins/hint_factory.py
+++ plugins/hint_factory.py
@@ -135,8 +135,6 @@ class SimpleHintFactory(object):
             return problem_node
             
     def hint_exists(self,problem_string,state_string):
-        import pdb; pdb.set_trace()
-        
         state_hash = self.hash_string(state_string)
         node = self.db.get_indexed_node("problems_index","start_string",state_string)
         if not node:
