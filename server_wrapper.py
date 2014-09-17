ee2ab50f223e8c22e0602908368531705b26d30a
bmsauer <brianmsauer@gmail.com>
2014-09-17 11:12:23 -0400

    changed server wrapper

--- server_wrapper.py
+++ server_wrapper.py
@@ -1,16 +1,17 @@
 """
-This script is used to start the Flask server on Windows.  In Unix, gunicorn 
+This script is used to start the Flask server on Windows.  In Unix, uWSGI 
 is used to start the server, which is coded using relative imports.  This is a 
 workaround so the relative imports are preserved in Windows.
 """
 
-#from server import app
-#from server.settings import settings
-
+import os
 from environment.settings_manager import SettingsManager
-settings_manager = SettingsManager.init_instance('production')
-settings = SettingsManager.get_server_settings()
 
+try:
+    settings_manager = SettingsManager.init_instance(os.environ['HPIT_ENV'])
+except KeyError:
+    settings_manager = SettingsManager.init_instance('debug')
+    
 from server.views.api import *
 from server.views.dashboard import *
 from server.app import ServerApp
@@ -18,4 +19,4 @@ from server.app import ServerApp
 server_app = ServerApp.get_instance()
 server_app.bootstrap_user()
 
-server_app.app.run(port = int(settings.HPIT_BIND_PORT))
+server_app.app.run(port = int(settings_manager.HPIT_BIND_PORT))
