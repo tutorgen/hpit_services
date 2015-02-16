bc18930b0366c6e8e4fcce91cc8c90970f19a992
bmsauer <brianmsauer@gmail.com>
2015-02-16 13:42:30 -0500

    Boredom detector new methods and tests

--- hpit/plugins/boredom_detector.py
+++ hpit/plugins/boredom_detector.py
@@ -16,6 +16,10 @@ from datetime import datetime
 
 import math
 
+class BoredomParameterException(Exception):
+    #thrown when a message does not have the required parameters for a boredom model 
+    pass
+
 class BoredomDetectorPlugin(Plugin):
     
     def __init__(self, entity_id, api_key, logger, args = None):
@@ -25,6 +29,13 @@ class BoredomDetectorPlugin(Plugin):
         self.db = self.mongo[settings.MONGO_DBNAME].hpit_boredom_detection
         
         self.RECORD_THRESHOLD = 5
+        self.threshold = .75 #this is the value the probability must be above to return "true"
+        
+        self.boredom_models = {
+            "simple":self._simple_boredom_calculation,
+            "complex":self._complex_boredom_calculation
+        }
+        self.boredom_calculation = self._simple_boredom_calculation
         
         if args:
             try:
@@ -39,15 +50,26 @@ class BoredomDetectorPlugin(Plugin):
         super().post_connect()
         
         self.subscribe({
+            "tutorgen.set_boredom_threshold":self.set_boredom_threshold,
+            "tutorgen.set_boredom_model":self.set_boredom_model,
             "tutorgen.update_boredom":self.update_boredom_callback,
             "tutorgen.boredom_transaction":self.transaction_callback_method
         })
         
-    def boredom_calculation(self,student_id,time):
-            
+    def _simple_boredom_calculation(self,message):
+        
+        try:
+            student_id = str(message["student_id"])
+        except KeyError:
+            raise BoredomParameterException("Simple boredom model requires a 'student_id'")
+        
+        
+        #this is an example of a boredom model.  it should take a message as its params, and return a float
         bored = False
 
-
+        student_id = message["student_id"]
+        time = message["time_created"]
+        
         #"Thu, 28 Nov 2013 22:28:43 GMT"
         insert_time = datetime.strptime(time, "%a, %d %b %Y %H:%M:%S GMT")
 
@@ -82,24 +104,96 @@ class BoredomDetectorPlugin(Plugin):
             if abs(dt_mean - ((records[0]["time"] - records[1]["time"]).total_seconds())) > abs(dt_mean - dt_std_dev):
                 bored = True
         
-        return bored
+        if bored == True:
+            return 1.0
+        else:
+            return 0.0
+            
+    def _complex_boredom_calculation(self,message):
+        """
+        TODO: put the more complex boredom calculation here"
+        """
+        return 1.0
+    
+    def set_boredom_threshold(self,message):
+        if self.logger:
+            self.send_log_entry("RECV: set_boredom_threshold with message: " + str(message))
+            
+        try:
+            threshold = message["threshold"]
+        except KeyError:
+            self.send_response(message["message_id"],{
+               "error":"set_boredom_threshold requires a 'threshold'",
+            })
+            return
+            
+        if threshold < 0 or threshold >1:
+            self.send_response(message["message_id"],{
+               "error":"threshold must be decimal value between 0 and 1.",
+            })
+            return
+            
+            
+        self.threshold = threshold
         
+        self.send_response(message["message_id"],{
+            "status":"OK",
+        })
+        
+    def set_boredom_model(self,message):
+        if self.logger:
+            self.send_log_entry("RECV: set_boredom_threshold with message: " + str(message))
+            
+        try:
+            model_name = message["model_name"]
+        except KeyError:
+            self.send_response(message["message_id"],{
+               "error":"set_boredom_model requires a 'model_name'",
+            })
+            return
+        if model_name in self.boredom_models:
+            self.boredom_calculation = self.boredom_models[model_name]
+        else:
+            self.send_response(message["message_id"],{
+               "error":"set_boredom_model unknown 'model_name'",
+            })
+            return
+        
+        self.send_response(message["message_id"],{
+            "status":"OK",
+        })
+    
         
     def update_boredom_callback(self,message):
         if self.logger:
             self.send_log_entry("RECV: update_boredom with message: " + str(message))
         
-        bored = False
+        return_type = "bool"
+        if "return_type" in message:
+            return_type = message["return_type"]
         
-        try:
-            student_id = str(message["student_id"])
-        except KeyError:
+        if return_type not in ["bool","decimal"]:
             self.send_response(message["message_id"],{
-               "error":"update_boredom requires a 'student_id'",
+               "error":"update_boredom 'return_type' must be 'bool' or 'decimal'",
             })
             return
-            
-        bored = self.boredom_calculation(student_id,message["time_created"])
+        
+        bored = None
+        try:
+            bored_prob = self.boredom_calculation(message)
+        except BoredomParameterException as e:
+            self.send_response(message["message_id"],{
+                    "error":str(e)
+                })
+            return
+
+        if return_type == "bool":
+            if bored_prob >= self.threshold:
+                bored = True
+            else:
+                bored = False
+        else:
+            bored = bored_prob
         
         self.send_response(message["message_id"],{
             "bored":bored,
@@ -130,6 +224,7 @@ class BoredomDetectorPlugin(Plugin):
         
         response = {}
         response["bored"] = bored
+        response["threshold"] = self.threshold
         response["responder"] = "boredom"
         self.send_response(message["message_id"],response)
         
--- tests/plugins/test_boredom_detector.py
+++ tests/plugins/test_boredom_detector.py
@@ -8,6 +8,7 @@ import shlex
 import json
 
 from hpit.plugins import BoredomDetectorPlugin
+from hpit.plugins import BoredomParameterException
 
 from hpit.management.settings_manager import SettingsManager
 settings = SettingsManager.get_plugin_settings()
@@ -56,81 +57,133 @@ class TestBoredomDetectorPlugin(unittest.TestCase):
         isinstance(ds.mongo,MongoClient).should.equal(True)
         isinstance(ds.db,Collection).should.equal(True)
         ds.db.full_name.should.equal("hpit_unit_test_db.hpit_boredom_detection")
-        
-    def test_update_boredom_callback(self):
+    
+    def test_simple_boredom_calculation(self):
         """
-        BoredomDetectorPlugin.update_boredom_callback() Test plan:
-            - try without student_id, should return error
+        BoredomDetectorPlugin._simple_boredom_calculation() Test plan:
+            - without student_id, should throw param exception
             - try without record, should be initted
             - add records, check if bored set to false on non- outlier, true on high outlier
-            - add records, check if bored set to false on non- outlier, true on high outlier
-            ** this test also tests update_moredom_callback
+            - add records, check if bored set to false on non- outlier, true on low outlier
         """
+        #invalid args
         time = datetime(2014,12,15,9,59)
         timestring = time.strftime("%a, %d %b %Y %H:%M:%S GMT")
+        msg =  {"message_id":"1","orig_sender_id":"2","time_created":timestring,"sender_entity_id":"999"}
+        self.test_subject._simple_boredom_calculation.when.called_with(msg).should.throw(BoredomParameterException)
         
-        #no args
-        msg = {"message_id":"1","orig_sender_id":"2","time_created":timestring,"sender_entity_id":"999"}
-        self.test_subject.update_boredom_callback(msg)
-        self.test_subject.send_response.assert_called_with("1",{
-            "error":"update_boredom requires a 'student_id'",      
-        })
-        self.test_subject.send_response.reset_mock()
-        
-        #first try, no record
-        msg["student_id"] = "123"
-        self.test_subject.update_boredom_callback(msg)
-        self.test_subject.send_response.assert_called_with("1",{
-            "bored":False      
-        })
-        self.test_subject.send_response.reset_mock()
-        
+        #first go, no records
+        msg = {"message_id":"1","student_id":"123","orig_sender_id":"2","time_created":timestring,"sender_entity_id":"999"}
+        self.test_subject._simple_boredom_calculation(msg).should.equal(0.0)
         self.test_subject.db.find_one({
             "student_id":"123",
             "time": time,
         }).should_not.equal(None)
-        
-        
-        #add some values, get bored set to true because of unusually high value
+    
+        #add some values, make sure still false
         for xx in range(0,5):
             time += timedelta(0,1)
             timestring = time.strftime("%a, %d %b %Y %H:%M:%S GMT")
             
             msg["time_created"] = timestring
-            self.test_subject.update_boredom_callback(msg)
-            self.test_subject.send_response.assert_called_with("1",{
-                "bored":False      
-            })
-        
+            self.test_subject._simple_boredom_calculation(msg).should.equal(0.0)
+            
+        #trip with a high value
         time += timedelta(0,5)
         timestring = time.strftime("%a, %d %b %Y %H:%M:%S GMT")
         msg["time_created"]=timestring
-        self.test_subject.update_boredom_callback(msg)  
-        self.test_subject.send_response.assert_called_with("1",{
-            "bored":True      
-        })
-        self.test_subject.send_response.reset_mock()
+        self.test_subject._simple_boredom_calculation(msg).should.equal(1.0)
         
-        #add some values, get bored set to true because of unusually low value
+        #reset, same thing but trip with low value
         self.test_subject.db.remove({})
         for xx in range(0,5):
             time += timedelta(0,5)
             timestring = time.strftime("%a, %d %b %Y %H:%M:%S GMT")
             
             msg["time_created"] = timestring
-            self.test_subject.update_boredom_callback(msg)
-            self.test_subject.send_response.assert_called_with("1",{
-                "bored":False      
-            })
+            self.test_subject._simple_boredom_calculation(msg).should.equal(0.0)
         
         time += timedelta(0,1)
         timestring = time.strftime("%a, %d %b %Y %H:%M:%S GMT")
         msg["time_created"] = timestring
-        self.test_subject.update_boredom_callback(msg)  
+        self.test_subject._simple_boredom_calculation(msg).should.equal(1.0)
+    
+    def test_complex_boredom_calculation(self):
+        """
+        BoredomDetectorPlugin._complex_boredom_calculation() Test plan:
+            - make sure it returns 1, in the short term
+        """
+        msg={}
+        self.test_subject._complex_boredom_calculation(msg).should.equal(1.0)
+    
+    def test_update_boredom_callback(self):
+        """
+        BoredomDetectorPlugin.update_boredom_callback() Test plan:
+            - set bogus return_type, should send error response
+            - mock boredom_calculation, throw BoredomParameterException, should return error
+            - don't set return_type
+                - mock boredom_calculation to return .7, threshold at .5, should return True
+                - mock boredom_calculation to return .3, threshold at .5, should return False
+                - mock boredom_calculation to return .5, threshold at .5, should return True
+            - set return_type to "decimal"
+                -mock boredom calculation to return .7, should return .7
+        """
+        time = datetime(2014,12,15,9,59)
+        timestring = time.strftime("%a, %d %b %Y %H:%M:%S GMT")
+        
+        #setting bogus return_type
+        msg = {"message_id":"1","student_id":"123","orig_sender_id":"2","time_created":timestring,"sender_entity_id":"999","return_type":"bogus"}
+        self.test_subject.update_boredom_callback(msg)
         self.test_subject.send_response.assert_called_with("1",{
-            "bored":True      
+                "error":"update_boredom 'return_type' must be 'bool' or 'decimal'",
         })
         self.test_subject.send_response.reset_mock()
+        
+        #mock boredom_calculation to throw exception
+        del msg["return_type"]
+        self.test_subject.boredom_calculation = MagicMock(side_effect = BoredomParameterException("Simple boredom model requires a 'student_id'"))
+        self.test_subject.update_boredom_callback(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+                "error":"Simple boredom model requires a 'student_id'",
+        })
+        self.test_subject.send_response.reset_mock()
+        
+        #return type bool, boredom .7, threshold .5
+        self.test_subject.threshold = 0.5
+        self.test_subject.boredom_calculation = MagicMock(return_value=0.7)
+        self.test_subject.update_boredom_callback(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+                "bored":True,
+        })
+        self.test_subject.send_response.reset_mock()
+        
+        #return type bool, boredom .3, threshold .5
+        self.test_subject.threshold = 0.5
+        self.test_subject.boredom_calculation = MagicMock(return_value=0.3)
+        self.test_subject.update_boredom_callback(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+                "bored":False,
+        })
+        self.test_subject.send_response.reset_mock()
+        
+        #return type bool, boredom .5, threshold .5
+        self.test_subject.threshold = 0.5
+        self.test_subject.boredom_calculation = MagicMock(return_value=0.5)
+        self.test_subject.update_boredom_callback(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+                "bored":True,
+        })
+        self.test_subject.send_response.reset_mock()
+        
+        msg["return_type"] = "decimal"
+        self.test_subject.update_boredom_callback(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+                "bored":0.5,
+        })
+        self.test_subject.send_response.reset_mock()
+        
+
+        
          
     def test_transaction_callback_method(self):
         """
@@ -148,7 +201,80 @@ class TestBoredomDetectorPlugin(unittest.TestCase):
             })
         
         
+    def test_set_boredom_threshold(self):
+        """
+        BoredomDetectorPlugin.set_boredom_threshold() Test plan:
+            - send without threshold, should return with error
+            - send with threshold below 0, should respond error
+            - send with threshold above 1, should respond error
+            - otherwise, response should be sent and threshold set
+        """
+        msg = {"message_id":"1","sender_entity_id":"888"}
+        self.test_subject.set_boredom_threshold(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+              "error":"set_boredom_threshold requires a 'threshold'",   
+        })
+        self.test_subject.send_response.reset_mock()
+         
+        msg["threshold"] = -1
+        self.test_subject.set_boredom_threshold(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+              "error":"threshold must be decimal value between 0 and 1.",   
+        })
+        self.test_subject.send_response.reset_mock()
+         
+        msg["threshold"] = 2
+        self.test_subject.set_boredom_threshold(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+             "error":"threshold must be decimal value between 0 and 1.",   
+        })
+        self.test_subject.send_response.reset_mock()
+         
+        msg["threshold"] = 1
+        self.test_subject.set_boredom_threshold(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+             "status":"OK",  
+        })
+        self.test_subject.send_response.reset_mock()
+         
+        msg["threshold"] = 0
+        self.test_subject.set_boredom_threshold(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+              "status":"OK",   
+        })
+        self.test_subject.send_response.reset_mock()
+         
+        self.test_subject.threshold.should.equal(0)
+     
+    def test_set_model_threshold(self):
+        """
+        BoredomDetectorPlugin.set_boredom_model() Test plan:
+           - send without model, shoudl reply error
+           - send bogus model, should reply with error
+           - send valid model, ensure that status is ok and bodel was set
+        """
+        msg = {"message_id":"1","sender_entity_id":"888"}
+        self.test_subject.set_boredom_model(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+            "error":"set_boredom_model requires a 'model_name'",          
+        })
+        self.test_subject.send_response.reset_mock()
         
+        msg["model_name"] = "bogus"
+        self.test_subject.set_boredom_model(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+            "error":"set_boredom_model unknown 'model_name'",          
+        })
+        self.test_subject.send_response.reset_mock()
+        
+        msg["model_name"] = "simple"
+        self.test_subject.set_boredom_model(msg)
+        self.test_subject.send_response.assert_called_with("1",{
+            "status":"OK",        
+        })
+        self.test_subject.send_response.reset_mock()
+        
+        self.test_subject.boredom_calculation.should.equal(self.test_subject._simple_boredom_calculation)
             
         
         
